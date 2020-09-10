5110603bd7dda5147cba2f5b352538f523c8b56b
Abhishek Singh <abhishek3571@gmail.com>
2020-09-10 17:19:39 +0530

    Completed the Introduction to components from Deborah Kurata Getting started tutorials

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+  `<div><h1>{{pageTitle}}</h1>
+  <pm-products></pm-products>
+  </div>`
 })
+
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  [x: string]: any;
+  // tslint:disable-next-line: no-inferrable-types
+  pageTitle: string = 'Acme Product Management';
 }
+
+
+
