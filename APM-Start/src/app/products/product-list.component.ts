0db74967c54bb84b51487d7a3c264c02a5fa4319
Abhishek Singh <abhishek3571@gmail.com>
2020-09-11 05:21:49 +0530

    Completed Templates, Interpolation and Directives from Deborah Kurata Angular: Getting started tutorials

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -10,5 +10,35 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
   // tslint:disable-next-line: no-inferrable-types
   pageTitle: string = 'Product List!';
+  imageWidth = 50;
+  imageMargin = 2;
+  // tslint:disable-next-line: no-inferrable-types
+  showImage: boolean = false;
+  products: any[] = [
+    {
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2019',
+      description: '15 gallon capacity rolling garden cart',
+      price: 132.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png'
+    },
+    {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2019',
+      description: 'Curved claw steel hammer',
+      price: 89.9,
+      starRating: 4.8,
+      imageUrl: 'assets/images/hammer.png'
+    },
+  ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+   }
 
 }
