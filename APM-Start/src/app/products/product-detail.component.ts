d9699e913deae6d78ce93bac41fcb57e83664481
Abhishek Singh <abhishek3571@gmail.com>
2020-09-12 21:05:50 +0530

    Completed the Navigation & Routing Additional Techniques and Angular Modules from Deborah Kurata angular tutorials

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -10,7 +11,25 @@ export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit(): void {}
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'May 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 89.9,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
